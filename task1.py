13081d592f8fcc8d855b7dd9b836f03509f34ce3
Amanstu799 <amankumarstu79@gmail.com>
2026-02-08 11:12:50 +0530

    assignment

---
+++ task1.py
@@ -0,0 +1,5 @@
+x = int(input("enter your number"))
+if x%2==0 :
+    print(f"{x} is even number")
+else:
+    print(f"{x} is odd number")    
---
+++ task2.py
@@ -0,0 +1,5 @@
+sum = 0
+for i in range(1,50):
+    sum += i
+
+print(f"sum of number from 1 to 50 is {sum}")
